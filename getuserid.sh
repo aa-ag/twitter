e4baa3852c7f8b9db0b0ed544c0f427986abe391
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-29 14:22:01 -0500

    Get curl response inside a variable

--- getuserid.sh
+++ getuserid.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 read -p 'Enter your username: ' varu
 
-read -sp 'enter your bearer token: ' vart
+read -sp 'Enter your bearer token: ' vart
 
-curl --request GET https://api.twitter.com/2/users/by/username/"$varu" --header 'Authorization: Bearer '"$vart"
+echo '\n'
+
+RES=$(curl --request GET https://api.twitter.com/2/users/by/username/"$varu" --header 'Authorization: Bearer '"$vart")
+
+echo $RES
+
+echo '\n'
