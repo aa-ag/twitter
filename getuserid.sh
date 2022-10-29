0f7cac991cc61e223bc13ae26750506107f0f65e
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-29 14:34:04 -0500

    Parse id from response and rename RES to USERID

--- getuserid.sh
+++ getuserid.sh
@@ -5,8 +5,9 @@ read -sp 'Enter your bearer token: ' vart
 
 echo '\n'
 
-RES=$(curl --request GET https://api.twitter.com/2/users/by/username/"$varu" --header 'Authorization: Bearer '"$vart")
+USERID=$(curl --request GET https://api.twitter.com/2/users/by/username/"$varu" \
+--header 'Authorization: Bearer '"$vart" | jq -r '.data.id')
 
-echo $RES
+echo $USERID
 
 echo '\n'
