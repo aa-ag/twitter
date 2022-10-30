9bbc955a7205f85c903c17e68952ea9e20c17c5d
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-30 09:46:08 -0500

    Get operation using .env variables instead

--- getuserid.sh
+++ getuserid.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-# read -p 'Enter your username: ' varu
-
-# read -sp 'Enter your bearer token: ' vart
-
-# echo '\n'
-
 source .env
-echo $USERNAME
-echo $BEARER_TOKEN
 
-# USERID=$(curl --request GET https://api.twitter.com/2/users/by/username/"$varu" \
-# --header 'Authorization: Bearer '"$vart" | jq -r '.data.id')
+USERID=$(curl --request GET https://api.twitter.com/2/users/by/username/$USERNAME \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data.id')
 
-# echo $USERID
+echo $USERID
 
-# echo '\n'
+echo '\n'
