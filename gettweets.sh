6cf9e0477704308082150517838d436d3921fa9b
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-31 19:01:22 -0500

    Add jq logic to parse result_count from response

--- gettweets.sh
+++ gettweets.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source .env
 
-curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
---header 'Authorization: Bearer '$BEARER_TOKEN
+TWEET_COUNT=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.meta.result_count')
+
+echo '\n'
+
+echo $TWEET_COUNT
+
+echo '\n'
