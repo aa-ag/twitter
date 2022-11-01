182ffd98274d46ee662999d72bacdeb611ecfc56
aa-ag <aaron.aguerrevere@gmail.com>
2022-10-31 19:06:02 -0500

    Test jq '.'

--- gettweets.sh
+++ gettweets.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source .env
+curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq '.'
 
-TWEET_COUNT=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.meta.result_count')
+# TWEET_COUNT=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
+# --header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.meta.result_count')
 
-echo '\n'
+# echo '\n'
 
-echo '@'$USERNAME ' has tweeted ' $TWEET_COUNT 'times.'
+# echo '@'$USERNAME ' has tweeted ' $TWEET_COUNT 'times.'
 
-echo '\n'
+# echo '\n'
