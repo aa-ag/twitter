39f03d5316f192b3de811e5d69c3cf54ef64d2c5
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-02 19:37:41 -0500

    Parse id from returned data using jq

--- someoneelse.sh
+++ someoneelse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source .env
 
-curl --request GET https://api.twitter.com/2/users/by/username/elonmusk --header 'Authorization: Bearer '$BEARER_TOKEN
+curl --request GET https://api.twitter.com/2/users/by/username/elonmusk \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data.id'
 
