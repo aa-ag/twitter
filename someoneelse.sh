65bd16509930cc33f930dd6fa5798f87f2b9933c
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-02 19:41:30 -0500

    Response of someone else's count working almost as expected, it's not the entire timeline, there's likely pagination

--- someoneelse.sh
+++ someoneelse.sh
@@ -6,4 +6,11 @@ HANDLE=$"elonmusk"
 SOMEONESID=$(curl --request GET https://api.twitter.com/2/users/by/username/$HANDLE \
 --header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data.id')
 
-echo $SOMEONESID
+TWEET_COUNT=$(curl --request GET 'https://api.twitter.com/2/users/'$SOMEONESID'/tweets' \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.meta.result_count')
+
+echo '\n'
+
+echo '@'$HANDLE ' has tweeted ' $TWEET_COUNT 'times.'
+
+echo '\n'
