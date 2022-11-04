89ecbbc05e6dfbcdda54a2031fa66041b0eb2029
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-03 20:52:21 -0500

    Add curl to count endpoint

--- count.sh
+++ count.sh
@@ -2,3 +2,6 @@
 source .env
 
 HANDLE=$"elonmusk"
+
+curl --request GET 'https://api.twitter.com/2/tweets/counts/recent?query=from%3ATwitterDev&granularity=day' \
+--header 'Authorization: Bearer '$BEARER_TOKEN
