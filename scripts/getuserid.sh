a76fbdb460677f85d69d6803f7713e81e25e36fa
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-06 09:32:12 -0600

    Decouple handle from env variable USERNAME

--- scripts/getuserid.sh
+++ scripts/getuserid.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source .env
 
-USERID=$(curl --request GET https://api.twitter.com/2/users/by/username/$USERNAME \
+HANDLE="elonmusk"
+
+USERID=$(curl --request GET https://api.twitter.com/2/users/by/username/$HANDLE \
 --header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data.id')
 
 echo $USERID
