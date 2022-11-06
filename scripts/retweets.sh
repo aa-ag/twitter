719e9a9c970d673ae3f09bc8ff930bbc7edd51f6
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-05 23:57:55 -0500

    Fix for loop by making variable all caps & deleting ..

--- scripts/retweets.sh
+++ scripts/retweets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source .env
 
-for tweet in '1480543699620618240'.. '1475963847076823040'
+for TWEET in '1480543699620618240' '1475963847076823040'
 do
     curl --request GET \
-    'https://api.twitter.com/2/tweets/'$tweet\
+    'https://api.twitter.com/2/tweets/'$TWEET\
     '/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
     --header 'Authorization: Bearer '$BEARER_TOKEN
 done
