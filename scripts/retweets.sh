e5df523b297d30a833bceef3d669630986d97999
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-05 23:56:30 -0500

    Add for loop for each ID

--- scripts/retweets.sh
+++ scripts/retweets.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 source .env
 
-my_tweets = [
-    '1480543699620618240', '1475963847076823040'
-]
-
-
-curl --request GET \
-'https://api.twitter.com/2/tweets/1354143047324299264/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
---header 'Authorization: Bearer '$BEARER_TOKEN
+for tweet in '1480543699620618240'.. '1475963847076823040'
+do
+    curl --request GET \
+    'https://api.twitter.com/2/tweets/'$tweet\
+    '/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
+    --header 'Authorization: Bearer '$BEARER_TOKEN
+done
