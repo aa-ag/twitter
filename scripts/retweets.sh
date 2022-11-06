455fe98efe57e7c4f62cacf3514f2bcb8a9e414c
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-06 09:30:01 -0600

    Get only text of each tweet

--- scripts/retweets.sh
+++ scripts/retweets.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 source .env
 
+TWEETS = ''
+
 for TWEET in '1480543699620618240' '1475963847076823040'
 do
     TEXT=$(curl --request GET \
     'https://api.twitter.com/2/tweets/'$TWEET\
     '/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
     --header 'Authorization: Bearer '$BEARER_TOKEN  | jq -r .data.text)
-    echo $TEXT
+    TWEETS+=$TEXT'\n'
 done
+
+echo $TWEETS
