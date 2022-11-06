4a9fed1226931336289b1ba55885b7428b35b16b
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-06 09:23:01 -0600

    Get curl GET into TEXT variable

--- scripts/retweets.sh
+++ scripts/retweets.sh
@@ -3,8 +3,9 @@ source .env
 
 for TWEET in '1480543699620618240' '1475963847076823040'
 do
-    curl --request GET \
+    TEXT=$(curl --request GET \
     'https://api.twitter.com/2/tweets/'$TWEET\
     '/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
-    --header 'Authorization: Bearer '$BEARER_TOKEN  | jq -r .data.text
+    --header 'Authorization: Bearer '$BEARER_TOKEN  | jq -r .data.text)
+    echo $TEXT
 done
