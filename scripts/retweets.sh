e17e42ac43bb9013e5336cd4766434f4440ebe81
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-05 23:52:28 -0500

    Add IDs for each of my actual own tweets

--- scripts/retweets.sh
+++ scripts/retweets.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source .env
 
+my_tweets = [
+    '1480543699620618240', '1475963847076823040'
+]
+
+
 curl --request GET \
 'https://api.twitter.com/2/tweets/1354143047324299264/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
 --header 'Authorization: Bearer '$BEARER_TOKEN
