87ed1bc259400e5796355e5daa277f19fa9e7d9a
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-07 20:07:59 -0600

    Add endpoint authorization stuff from documentation TBD

--- scripts/blocks.sh
+++ scripts/blocks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
 
-curl --request GET 'https://api.twitter.com/2/users/'$USERID'/blocking?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at' \
---header 'Authorization: Bearer '$BEARER_TOKEN
+curl --request GET 'https://api.twitter.com/2/users/'$USERID'/blocking' \
+--header 'authorization: OAuth oauth_consumer_key="'$API_KEY'", oauth_nonce="OAUTH_NONCE", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="OAUTH_TIMESTAMP", oauth_token="ACCESS_TOKEN", oauth_version="1.0"'
