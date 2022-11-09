675612cfa0d9b7dacb121bfe48428f55059adfc7
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-08 18:35:51 -0600

    Get metrics for a tweet tweeted by me

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
 
-curl --request GET 'https://api.twitter.com/2/tweets/1263150595717730305?tweet.fields=public_metrics' \
+curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
 --header 'Authorization: Bearer '$BEARER_TOKEN
