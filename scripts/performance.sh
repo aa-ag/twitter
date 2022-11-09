17c9c9e33f35ab4d7df335f143ac841da0fde018
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-08 18:38:00 -0600

    Parse public_metrics from response

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -2,6 +2,6 @@
 source .env
 
 METRICS=$(curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN)
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r ".data.public_metrics")
 
 echo $METRICS
