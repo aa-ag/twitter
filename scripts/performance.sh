60f0272ec65376aab0df287e44fa4e92d94e5d3d
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-08 18:36:55 -0600

    Store public metrics from a tweet in METRICS

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source .env
 
-curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN
+METRICS=$(curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
+--header 'Authorization: Bearer '$BEARER_TOKEN)
+
+echo $METRICS
