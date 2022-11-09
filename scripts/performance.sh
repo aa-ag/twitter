ed830612d5de6278effa4cc8c27dfb5d672ec743
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-08 18:49:14 -0600

    Remove temp store to have json pretty print in cli for readability

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source .env
 
-METRICS=$(curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq -r ".data.public_metrics")
 
-echo $METRICS
+curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
