2210e1c83f0fc1d72470c16a8d6d77249ca95930
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-09 21:07:53 -0600

    Add for loop logic to get metrics for several tweets

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source .env
 
-
-curl --request GET 'https://api.twitter.com/2/tweets/1480543699620618240?tweet.fields=public_metrics' \
+for TWEET in "1589024671252770833" "1589023612635582465"
+do 
+curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
 --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+done
