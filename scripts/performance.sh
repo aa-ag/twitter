670b745ea389e6cec2bdc2762cc7947a7ea9c156
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-09 21:15:27 -0600

    Add linebreak & now display both the tweet's text + its performance in the CLI

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -4,8 +4,10 @@ source .env
 for TWEET in "1480543699620618240" "1475963847076823040"
 do 
 curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
---header 'Authorization: Bearer '$BEARER_TOKEN
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.text"
 
-# curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
-# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+
+echo '\n'
 done
