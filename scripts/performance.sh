7482ce711fd7f42fdeb70463aa387ebabded93ba
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-09 21:14:30 -0600

    First get the actual tweet, then its performance

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 source .env
 
-for TWEET in "1589024671252770833" "1589023612635582465"
+for TWEET in "1480543699620618240" "1475963847076823040"
 do 
-curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
+--header 'Authorization: Bearer '$BEARER_TOKEN
+
+# curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
+# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
 done
