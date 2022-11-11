4e0d388efbd3860aa70997090168ffb91f215c36
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-10 21:11:01 -0600

    Iterate over tweets one at the time printing ID

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -4,10 +4,9 @@ source .env
 TWEETS=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
 --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data[].id")
 
-echo $TWEETS
-
-# for TWEET in "1480543699620618240" "1475963847076823040"
-# do 
+for TWEET in $TWEETS
+do
+echo $TWEET 
 # curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
 # --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.text"
 
@@ -15,4 +14,4 @@ echo $TWEETS
 # --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
 
 # echo '\n'
-# done
+done
