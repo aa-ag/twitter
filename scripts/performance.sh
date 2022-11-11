a25aa1a638f496cb759275cf951d68e18782ae7d
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-10 21:06:37 -0600

    Get tweets with an ID first

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 source .env
 
-for TWEET in "1480543699620618240" "1475963847076823040"
-do 
-curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.text"
+TWEETS=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
+--header 'Authorization: Bearer '$BEARER_TOKEN)
 
-curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+echo $TWEETS
 
-echo '\n'
-done
+# for TWEET in "1480543699620618240" "1475963847076823040"
+# do 
+# curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
+# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.text"
+
+# curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
+# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+
+# echo '\n'
+# done
