86075caf647bc0fda87c63c2e1938ece3ef24d8a
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-12 11:34:12 -0600

    Test with documentation example, working as expected

--- scripts/likes.sh
+++ scripts/likes.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 source .env
 
-DONE=1
+read var1 var2 var3 <<< $(curl -s 'https://api.github.com/repos/torvalds/linux' | jq -r '.id, .name, .full_name')
 
-while (( $DONE != 0 ));
-do
-    R=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/liked_tweets' \
-    --header 'Authorization: Bearer '$BEARER_TOKEN | jq)
-    echo $R
-    ((DONE = 0))
-done
+echo $var1
+# read r meta < <(echo $(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/liked_tweets' \
+#     --header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data .meta'))
+#  echo $meta
 
-echo "\n ^^^ Done."
+# running=1
+
+# while (( $running != 0 ));
+# do
+    # r=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/liked_tweets' \
+    # --header 'Authorization: Bearer '$BEARER_TOKEN)
+    # # echo $r
+    # meta=$( jq -r '.meta' <<< "{$r}" )
+    # echo $meta
+    # ((running = 0))
+# done
+
+# echo "\n ^^^ Done."
