f57d49c5826f7d3b81ac8742a2b6c059a1321d52
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-11 19:41:43 -0600

    Have jq parse .data[] ids recursively

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -2,16 +2,17 @@
 source .env
 
 TWEETS=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data[].id")
+--header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data[].id')
 
-for TWEET in $TWEETS
-do
+echo $TWEETS
+# for TWEET in $TWEETS
+# do
 # echo $TWEET 
 # curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
-# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.text"
+# --header 'Authorization: Bearer '$BEARER_TOKEN
 
-curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
---header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
+# curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET'?tweet.fields=public_metrics' \
+# --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
 
-echo '\n'
-done
+# echo '\n'
+# done
