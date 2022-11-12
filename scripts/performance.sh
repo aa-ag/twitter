6fbdd21baa3dc21c1ba8aae245f20331c92890f7
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-11 19:42:56 -0600

    Loop thru tweets, looking correct

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -4,10 +4,9 @@ source .env
 TWEETS=$(curl --request GET 'https://api.twitter.com/2/users/'$USERID'/tweets' \
 --header 'Authorization: Bearer '$BEARER_TOKEN | jq -r '.data[].id')
 
-echo $TWEETS
-# for TWEET in $TWEETS
-# do
-# echo $TWEET 
+for TWEET in $TWEETS
+do
+echo $TWEET 
 # curl --request GET 'https://api.twitter.com/2/tweets/'$TWEET \
 # --header 'Authorization: Bearer '$BEARER_TOKEN
 
@@ -15,4 +14,4 @@ echo $TWEETS
 # --header 'Authorization: Bearer '$BEARER_TOKEN | jq ".data.public_metrics"
 
 # echo '\n'
-# done
+done
