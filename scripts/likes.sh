11b585ffed42093391c90905f7001a644184aec4
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-14 19:18:21 -0600

    Updating the URL with each loop

--- scripts/likes.sh
+++ scripts/likes.sh
@@ -6,16 +6,18 @@ running=1
 total_likes=0
 
 url_and_endpoint='https://api.twitter.com/2/users/'$USERID'/liked_tweets'
+echo $url_and_endpoint
 
 while (( $running != 0 ));
 do
     r=$(curl --request GET $url_and_endpoint --header 'Authorization: Bearer '$BEARER_TOKEN) || exit
-    # echo $r
-    # likes_count=$(jq -r '.meta.result_count' <<< "$r")
-    # total_likes+=likes_count
+    likes_count=$(jq -r '.meta.result_count' <<< "$r")
+    total_likes+=likes_count
     next_token=$(jq -r '.meta.next_token' <<< "$r")
-    echo $next_token
-    ((running = 0))
+    url_and_endpoint=$url_and_endpoint'&next_token='$next_token
+    echo $url_and_endpoint
+    # ((running = 0))
+    # echo '\n'
 done
 
 echo '@'$USERNAME' has liked '$likes_count' tweets.'
