a18cb13909e9e3d68ff1c26c0974d97a124b4873
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-14 19:25:58 -0600

    Parsing next_token and passing the urls with it to the loop

--- scripts/likes.sh
+++ scripts/likes.sh
@@ -5,19 +5,23 @@ running=1
 
 total_likes=0
 
-url_and_endpoint='https://api.twitter.com/2/users/'$USERID'/liked_tweets'
-echo $url_and_endpoint
+original_url='https://api.twitter.com/2/users/'$USERID'/liked_tweets'
+url_and_endpoint=$original_url
 
 while (( $running != 0 ));
 do
     r=$(curl --request GET $url_and_endpoint --header 'Authorization: Bearer '$BEARER_TOKEN) || exit
+    
     likes_count=$(jq -r '.meta.result_count' <<< "$r")
+    
     total_likes+=likes_count
+    
     next_token=$(jq -r '.meta.next_token' <<< "$r")
-    url_and_endpoint=$url_and_endpoint'&next_token='$next_token
+    url_and_endpoint=$original_url'&next_token='$next_token
     echo $url_and_endpoint
-    # ((running = 0))
-    # echo '\n'
+    
+    ((running = 0))
+    echo '\n'
 done
 
 echo '@'$USERNAME' has liked '$likes_count' tweets.'
